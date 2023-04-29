40a55853dd27adaf44e44cbedc6af991afe18f0e
Bhushan Kadam <101280175+Bhushan131191@users.noreply.github.com>
2023-04-29 12:00:26 +0530

    Add files via upload

---
+++ Assignment13/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+export interface DialogData {
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+
+
+}
